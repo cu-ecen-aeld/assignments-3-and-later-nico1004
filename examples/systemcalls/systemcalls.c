38c6f783014a6bb807d7386ce98b60b0ed17f55d
Nico Röder <nico.roeder@live.de>
2023-03-13 10:03:45 +0100

    finished assignment 3-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,8 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+	int retval = system(cmd);	
+    return retval == 0;
 }
 
 /**
@@ -58,10 +58,15 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    	int err = true;
+    	fork();
+	execv(command[0], &command[1]);
+    	err = false;
+	wait(NULL);
 
-    va_end(args);
+	va_end(args);
 
-    return true;
+    return err;
 }
 
 /**
@@ -92,8 +97,24 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    	int err = true;
+	int kidpid;
+	int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	if (fd < 0) { perror("open"); abort(); }
+	switch (kidpid = fork()) {
+	 	case -1: perror("fork"); err = false; abort();
+  		case 0:
+		    	if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+		    	close(fd);
+		    	execvp(command[0], &command[1]); perror("execvp"); abort();
+			err = false;
+			wait(NULL);
+  		default:
+    			close(fd);
+			err=false;
+}
 
     va_end(args);
 
-    return true;
+    return err;
 }
